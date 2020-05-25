2c584ac38804f8829425ec8e910775fc14dc0672
Dakota Palmer <dakotapa@usc.edu>
2020-05-24 21:54:57 -0700

    Filled About me and Work Experience. Will fill in the rest of informatin on next push. Need to rethink my design strategy

--- flask-react-app/src/components/content/Workexp/Workexp.js
+++ flask-react-app/src/components/content/Workexp/Workexp.js
@@ -2,27 +2,65 @@ import React from 'react';
 import './Workexp.css'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 
+import Linquest from '../../../pictures/linquest.jpeg'
+import JPL from '../../../pictures/jpl.png'
+
 function Workexp() {
 
   return (
   	<div className="Workexp">
-    		<Jumbotron fluid>
-    			<h1> Work Experience </h1>
-    			<p>
-    				Content information goes here Content information goes here
-    				Content information goes here Content information goes here
-    				Content information goes here Content information goes here
-    				Content information goes here Content information goes here
-                    Content information goes here Content information goes here
-                    Content information goes here Content information goes here
-                    Content information goes here Content information goes here
-                    Content information goes here Content information goes here
-                    Content information goes here Content information goes here
-                    Content information goes here Content information goes here
-                    Content information goes here Content information goes here
-                    Content information goes here Content information goes here
-    			</p> 
-    		</Jumbotron>
+
+    	<h2> Work Experience </h2> <br/>
+        <div className="Linquest">
+
+            <img src={Linquest} alt="Linquest logo"/>
+            <div className="LinquestDescription">
+            <h3> Linquest October 2018 - March 2020 </h3>
+            <h5> Semester: Spring 2019 - Spring 2020 <br/>Position: Software Engineer Intern </h5> 
+            <h5> Achievements: </h5>
+            <ul>
+                <li> Practice agile development methodologies in a small software team.</li>
+                <li> Utilize Jasmine testing framework for unit tests for backend Python Pyramid
+                business functionality.</li>
+                <li> Deploy docker containers to implement end to end testing</li>
+                <li> Integrate Bokeh NetworkX Graphs into Angular using backend to pull 
+                    Jira Data from Rest API to graph dependencies between Jira programs</li>
+            </ul>
+
+
+            <h5> Semester: Fall 2018 <br/>Position: Cyber Security Systems Engineer Intern </h5> 
+            <h5> Achievements: </h5>
+            <ul>
+                <li> Assist in the completion of various tasks regarding cyber tests and 
+                evaluations and construction of technical requirements documents.</li>
+                <li> Research the Risk Management Framework and constructed framework 
+                policies with management tailored to DOD systems.</li>
+                <li> Conduct meetings and collaborate with government contacts in
+                 construction of an Interface Control Document.</li>
+            </ul>
+
+            </div>
+        </div>
+        <div className="JPL">
+            <img src={JPL} alt="JPL logo"/>
+            <div className="JPLDescription">
+            <h5> Semester: Fall 2015 <br/>Position: Aerospace Scholar Intern </h5> 
+            <h5> Achievements: </h5>
+            <ul>
+                <li> C++ Programming of a rover which simulates all phases of a flight 
+                program for a MARS exploration rover that conducts a sample return, emergency
+                assist of other rovers, and accomplishes science objectives as well as 
+                habitable exploration documentary.</li>
+                <li> Held position of team that helped coordinate maneuverability functionalities
+                 and data deliveries to project management.</li>
+                <li> Interacted with NASA personnel and engineers to build and test two prototypes
+                 of a planetary sample return rover.</li>
+            </ul>
+            </div>
+        </div>
+
+
+
   	</div>
   );
 }
