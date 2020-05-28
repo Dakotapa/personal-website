37c3872d6dc4e06ea77c1c1ed421e224ea29373e
Dakota Palmer <dakotapa@usc.edu>
2020-05-28 16:50:26 -0700

    Fixed some issues with footer sticking to top is missing ml-auto child

--- flask-react-app/src/components/Footer.js
+++ flask-react-app/src/components/Footer.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Footer.css'
 import { Navbar, Nav } from 'react-bootstrap';
-
 import CopyRight from '../pictures/copyright.png'
+
 function Footer() {
 
   return (
-    	<Navbar bg="dark" className="header" variant="dark">
-    		<Navbar.Brand id="footerCrText" href="">
-          <img id="crImg" src={CopyRight} alt="cr icon" /> 2020 Dakota Palmer
-        </Navbar.Brand>
+    	<Navbar bg="dark" variant="dark">
+    		<Navbar.Brand id="footerCrText" href=""><img id="crImg" src={ CopyRight }/> 2020 Dakota Palmer</Navbar.Brand>
+    
+          <Nav className="ml-auto">
+      			<Nav.Link>
+              
+            </Nav.Link>
+      		</Nav>
       </Navbar>
   );
 }
